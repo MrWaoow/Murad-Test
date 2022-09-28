02abc76bd5f31d12d60d5e60cabb53795ea36cdb
MrWaoow <mr.waoow97@gmail.com>
2022-09-29 00:21:08 +0200

    Created info section and view more Pop up block

---
+++ assets/js/PopUp.js
@@ -0,0 +1,10 @@
+const closeBtn = document.getElementById("close");
+const modalMenu = document.getElementById("modal");
+const modalBtn = document.querySelector(".modal__button");
+//const viewMoreBtn = document.querySelectorAll(".contentBx a")
+
+const viewMore = ()=>{modalMenu.classList.add("show")}
+
+//viewMoreBtn.addEventListener("click",()=>{modalMenu.classList.add("show")});
+closeBtn.addEventListener("click",()=>{modalMenu.classList.remove("show")});
+modalBtn.addEventListener("click",()=>{modalMenu.classList.remove("show")});
