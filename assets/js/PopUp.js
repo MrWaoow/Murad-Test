a111776b37381fbe69a3e6cc7017f3b87b98585c
MrWaoow <mr.waoow97@gmail.com>
2022-10-15 02:29:05 +0200

    Popup Updated function "Hide popup when clicking outside"

--- assets/js/PopUp.js
+++ assets/js/PopUp.js
@@ -3,8 +3,18 @@ const modalMenu = document.getElementById("modal");
 const modalBtn = document.querySelector(".modal__button");
 //const viewMoreBtn = document.querySelectorAll(".contentBx a")
 
-const viewMore = ()=>{modalMenu.classList.add("show")}
+const viewMore = () => { modalMenu.classList.add("show") }
 
 //viewMoreBtn.addEventListener("click",()=>{modalMenu.classList.add("show")});
-closeBtn.addEventListener("click",()=>{modalMenu.classList.remove("show")});
-$("body").click(function(){ $("#modal").fadeOut(); });
+closeBtn.addEventListener("click", () => { modalMenu.classList.remove("show") });
+//hide it when clicking anywhere else except the popup and the trigger
+document.addEventListener("click", (event) => {
+    if (!$(event.target).parents().addBack().is('a')) {
+        modalMenu.classList.remove("show");
+    }
+});
+
+// Stop propagation to prevent hiding "#tooltip" when clicking on it
+modalMenu.addEventListener("click", (event) => {
+    event.stopPropagation();
+});
